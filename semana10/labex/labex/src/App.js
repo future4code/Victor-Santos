622366ab7922897fae0b9f966ce315c6cd4c7d98
Noguvictor <noguvictor@gmail.com>
2021-08-17 16:18:00 -0300

    LABEX (SEMANA10) - PRIMEIROS PASSOS

--- semana10/labex/labex/src/App.js
+++ semana10/labex/labex/src/App.js
@@ -2,6 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import { HomePage } from './Pages/HomePage';
 import { ListTripPage } from './Pages/ListTripPage';
+import { ApplicationFormPage } from './Pages/ApplicationFormPage';
+import { LoginPage } from './Pages/LoginPage';
+import { AdminHomePage } from './Pages/AdminHomePage';
+import { CreateTripPage } from './Pages/CreateTripPage';
+import { TripDetailsPage } from './Pages/TripDetailsPage';
 
 function App() {
   return (
@@ -10,6 +15,15 @@ function App() {
       <hr />
       <ListTripPage />
       <hr />
+      <ApplicationFormPage />
+      <hr/>
+      <LoginPage />
+      <hr/>
+      <AdminHomePage />
+      <hr/>
+      <CreateTripPage />
+      <hr/>
+      <TripDetailsPage />
     </div>
   );
 }
